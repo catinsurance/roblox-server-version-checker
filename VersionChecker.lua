8535fc26cf0b3c1f3861dbc0fea7171e93cf45a0
Maya <65452914+maya-bee@users.noreply.github.com>
2021-07-08 15:48:14 -0500

    Fix signal implementation again

--- VersionChecker.lua
+++ VersionChecker.lua
@@ -1,52 +1,54 @@
 -- Quenty Signal Class	
-local Signal = {}
-Signal.__index = Signal
-Signal.ClassName = "Signal"
+local Signal do
+	Signal = {}
+	Signal.__index = Signal
+	Signal.ClassName = "Signal"
 
---- Constructs a new signal.
--- @constructor Signal.new()
--- @treturn Signal
-function Signal.new()
-	local self = setmetatable({}, Signal)
+	--- Constructs a new signal.
+	-- @constructor Signal.new()
+	-- @treturn Signal
+	function Signal.new()
+		local self = setmetatable({}, Signal)
 
-	self._bindableEvent = Instance.new("BindableEvent")
-	self._argData = nil
-	self._argCount = nil -- Prevent edge case of :Fire("A", nil) --> "A" instead of "A", nil
+		self._bindableEvent = Instance.new("BindableEvent")
+		self._argData = nil
+		self._argCount = nil -- Prevent edge case of :Fire("A", nil) --> "A" instead of "A", nil
 
-	return self
-end
-
---- Fire the event with the given arguments. All handlers will be invoked. Handlers follow
--- Roblox signal conventions.
--- @param ... Variable arguments to pass to handler
--- @treturn nil
-function Signal:Fire(...)
-	self._argData = {...}
-	self._argCount = select("#", ...)
-	self._bindableEvent:Fire()
-	self._argData = nil
-	self._argCount = nil
-end
-
---- Connect a new handler to the event. Returns a connection object that can be disconnected.
--- @tparam function handler Function handler called with arguments passed when `:Fire(...)` is called
--- @treturn Connection Connection object that can be disconnected
-function Signal:Connect(handler)
-	if not (type(handler) == "function") then
-		error(("connect(%s)"):format(typeof(handler)), 2)
+		return self
 	end
 
-	return self._bindableEvent.Event:Connect(function()
-		handler(unpack(self._argData, 1, self._argCount))
-	end)
-end
+	--- Fire the event with the given arguments. All handlers will be invoked. Handlers follow
+	-- Roblox signal conventions.
+	-- @param ... Variable arguments to pass to handler
+	-- @treturn nil
+	function Signal:Fire(...)
+		self._argData = {...}
+		self._argCount = select("#", ...)
+		self._bindableEvent:Fire()
+		self._argData = nil
+		self._argCount = nil
+	end
 
---- Wait for fire to be called, and return the arguments it was given.
--- @treturn ... Variable arguments from connection
-function Signal:Wait()
-	self._bindableEvent.Event:Wait()
-	assert(self._argData, "Missing arg data, likely due to :TweenSize/Position corrupting threadrefs.")
-	return unpack(self._argData, 1, self._argCount)
+	--- Connect a new handler to the event. Returns a connection object that can be disconnected.
+	-- @tparam function handler Function handler called with arguments passed when `:Fire(...)` is called
+	-- @treturn Connection Connection object that can be disconnected
+	function Signal:Connect(handler)
+		if not (type(handler) == "function") then
+			error(("connect(%s)"):format(typeof(handler)), 2)
+		end
+
+		return self._bindableEvent.Event:Connect(function()
+			handler(unpack(self._argData, 1, self._argCount))
+		end)
+	end
+
+	--- Wait for fire to be called, and return the arguments it was given.
+	-- @treturn ... Variable arguments from connection
+	function Signal:Wait()
+		self._bindableEvent.Event:Wait()
+		assert(self._argData, "Missing arg data, likely due to :TweenSize/Position corrupting threadrefs.")
+		return unpack(self._argData, 1, self._argCount)
+	end
 end
 
 --- Disconnects all connected events to the signal. Voids the signal as unusable.
@@ -63,7 +65,6 @@ end
 
 -- Real code starts below
 local v = {}
-local _signal = require(script.Signal)
 
 local rs = game:GetService("RunService")
 local mps = game:GetService("MarketplaceService")
@@ -80,8 +81,8 @@ local UpToDateGame = true
 
 local CheckFrequency = 5
 
-v.GameUpdated = _signal.new()
-v.PlaceUpdated = _signal.new()
+v.GameUpdated = Signal.new()
+v.PlaceUpdated = Signal.new()
 
 function GetGameProductInfo()
 	local success, info = pcall(function()
